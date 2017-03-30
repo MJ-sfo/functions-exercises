e6e3ce10ca63e5053f01bfa4b3b0cf443ec4d83c
MJ-sfo <michaeljlaird@yahoo.com>
2017-03-29 20:13:44 -0700

    find area of circle

--- functions.js
+++ functions.js
@@ -1,7 +1,39 @@
 // TODO: add your solutions here!
 function addWords(first, second) {
-  var sntnc = first  + second;
-  console.log(sntnc) ;
-
+  var sentence = first  + second;
+  console.log(sentence) ;
+  return sentence;
 }
 addWords("John", "son");
+
+// 2. Repeat a phrase
+function display (phrase, numb) {
+  for (var i = 0; i < numb; i++) {
+    console.log(phrase);
+  }
+}
+display("looping 5 times", 5);
+
+// 3. Build your own Power function
+function powerBall (first, power) {
+  var result = 1;
+  for (var i = 1; i <= power; i++) {
+    result = result * first;
+  }
+  console.log(result);
+  return result;
+}
+powerBall(2, 4);
+
+// 4. Area of a circle: π r2
+// psudocode
+// input r
+// calculate
+// return Area
+
+function pieRadiusSq (radius) {
+  var area = radius * radius * 3.14;
+  console.log(area);
+  return area;
+}
+pieRadiusSq(10);
